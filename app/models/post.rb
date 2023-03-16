44b8afbda84e178bc9580db53d0e8f5a52adf60e
aowuor <angelaowuor@gmail.com>
2023-03-17 00:41:45 +0300

    commit tree 4bd5231757bd48634f05e35878ae4677b28c8991

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+    validate :title_true_facts
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: ["Fiction","Non-Fiction"]} 
+
+    
+
+    def title_true_facts
+        unless title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+            errors.add (:title, "Title not sufficiently clickbait-y")
+        end
+    end
+
+    def title_is_clickbait
+        if !title || (!title.include?("Won't Believe") && !title.include?("Secret") && !title.match?(/Top \d+/) && !title.include?("Guess"))
+          errors.add(:title, "is not sufficiently clickbait-y")
+        end
+      end
+
+
 end
